942dbb230641e78c4048756fde9e6344b7a1f58c
Gabriel Dezan <58042141+gabrieldezan@users.noreply.github.com>
2025-07-30 15:12:23 -0300

    Backup

--- wdadmin/config.php
+++ wdadmin/config.php
@@ -4,16 +4,16 @@ define("CLIENTE", "EZ Engenharia");
 define("URL_SITE", "https://ezengenharia.com.br/");
 
 /* CONFIG LOCAL */
-define("URL", "/ezengenharia/");
-define("SERVIDOR", "127.0.0.1");
-define("BANCO", "ezengenharia_site");
-define("USUARIO", "root");
-define("SENHA", "!@#gd94!@#");
-
-/* CONFIG SERVIDOR */
-// define("URL", "/");
+// define("URL", "/ezengenharia/");
 // define("SERVIDOR", "127.0.0.1");
 // define("BANCO", "ezengenharia_site");
-// define("USUARIO", "ezengenharia_admin");
-// define("SENHA", "i@YQ7QU^In80");
+// define("USUARIO", "root");
+// define("SENHA", "");
+
+/* CONFIG SERVIDOR */
+define("URL", "/");
+define("SERVIDOR", "127.0.0.1");
+define("BANCO", "ezengenharia_site");
+define("USUARIO", "ezengenharia_admin");
+define("SENHA", "i@YQ7QU^In80");
 ?>
